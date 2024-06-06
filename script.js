d56047ccbf62b35623b9ce63980452cef48c8dd7
smirnov <smirnov@alfasatcom.ru>
2024-06-06 16:14:35 +0300

    work with varibles

---
+++ script.js
@@ -0,0 +1,15 @@
+// lesson username
+let user='bob', age='35', sex='male'
+const person=user+' '+age+' '+sex;
+alert(person);
+
+let admin='John';
+let name=admin;
+const msg=`var name is ${name}`;
+console.log(msg);
+alert(msg);
+
+let planet='earth';
+let curUser='fedor';
+console.log(`current user is ${curUser}`);
+console.log(`our planet is ${planet}`);
