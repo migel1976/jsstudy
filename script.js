81e5f93db3acfec4f7b96f21ad95c78d6214739c
smirnov <smirnov@alfasatcom.ru>
2024-06-07 08:33:29 +0300

    преобразование типов

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
 // lesson username
-// Переменные
-let user='bob', age='35', sex='male'
-const person=user+' '+age+' '+sex;
-alert(person);
+// // Переменные
+// let user='bob', age='35', sex='male'
+// const person=user+' '+age+' '+sex;
+// alert(person);
 
-let admin='John';
-let name=admin;
-const msg=`var name is ${name}`;
-console.log(msg);
-alert(msg);
+// let admin='John';
+// let name=admin;
+// const msg=`var name is ${name}`;
+// console.log(msg);
+// alert(msg);
 
-let planet='earth';
-let curUser='fedor';
-console.log(`current user is ${curUser}`);
-console.log(`our planet is ${planet}`);
+// let planet='earth';
+// let curUser='fedor';
+// console.log(`current user is ${curUser}`);
+// console.log(`our planet is ${planet}`);
 
-//Типы данных
-console.log(typeof(5));
-console.log(typeof null);
+// //Типы данных
+// console.log(typeof(5));
+// console.log(typeof null);
 
-//alert, prompt
-let yourAge=prompt('сколько тебе лет');
-const lblAge=document.querySelector('.age');
-lblAge.innerHTML=yourAge;
+// //alert, prompt
+// let yourAge=prompt('сколько тебе лет');
+// const lblAge=document.querySelector('.age');
+// lblAge.innerHTML=yourAge;
 
-const res=confirm('я здесь БОСС!!!')
-const lblBoss=document.querySelector('.boss');
-lblBoss.innerHTML=res;
+// const res=confirm('я здесь БОСС!!!')
+// const lblBoss=document.querySelector('.boss');
+// lblBoss.innerHTML=res;
+
+//convert types
+let val=true;
+console.log(typeof val);
+console.log(typeof String(val));
+
+//численный преобразования
+console.log('6'/'3');
+console.log(null + 45);
+console.log(undefined + 76);
+
+//логические преобразования
+console.log(Boolean('hj'));
+console.log(Boolean(null));
+console.log(Boolean('0'));
+console.log(Boolean(0));
+
 
 
 
